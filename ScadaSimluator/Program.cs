5fbaf7032deb3156d519341a37d70b6a42ab2384
Vladimir <vzlobin2003@liost.ru>
2018-10-14 20:01:50 +0300

    Доработка

--- ScadaSimluator/Program.cs
+++ ScadaSimluator/Program.cs
@@ -4,19 +4,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
+using ScadaSimluator;
+using System.Threading;
 
 
 namespace ScadaSimluator
 {
     class Program
     {
+        public static MySqlConnection conn = DBUtils.GetDBConnection();
+        public static int timeInterval = 5000;
         static void Main(string[] args)
         {
+            Spt spt1 = new Spt(601, 90, 70, 5, 4, 6);
+            Console.WriteLine(spt1.ToList());
+            //подключение к Базе данных
             Console.WriteLine("Коннектимся...");
-            MySqlConnection conn = DBUtils.GetDBConnection();
-            try
+            
+
+            ConsoleKeyInfo cki = new ConsoleKeyInfo();
+            while (true)
             {
+                Console.WriteLine("итерация");
+                conn = DBUtils.GetDBConnection();
                 conn.Open();
+                spt1.qsum = GetQsumm(spt1);
+                spt1.Random();
+                SptTobBase(spt1);
+                Console.WriteLine(spt1.ToList());
+
+                conn.Close();
+                conn.Dispose();
+                conn = null;
+                Thread.Sleep(timeInterval);
+
+                if (Console.KeyAvailable == true)
+                {
+                    cki = Console.ReadKey(true);
+                    if (cki.Key == ConsoleKey.Q)
+                    {
+                        Console.WriteLine("Выход из потока");
+                        break;
+
+                    }
+                }
+
+            }
+            
+
+
+
+ /*           try
+            {
+               // conn.Open();
                 Console.WriteLine("Соединение установлено");
             }
             catch (Exception e)
@@ -26,28 +66,7 @@ namespace ScadaSimluator
 
             try
             {
-                string sql = "INSERT INTO cnldata (datetime, cnlnum, val, stat) VALUES(@dateTime, @cnlNum, @val, @stat) ON DUPLICATE KEY UPDATE datetime = @dateTime,  val = @val";
-                MySqlCommand cmd = conn.CreateCommand();
-                cmd.CommandText = sql;
-
-                MySqlParameter dateTimeParam = new MySqlParameter("@dateTime", MySqlDbType.String);
-                dateTimeParam.Value = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-                //dataTimeParam.Value = "2012-10-12 22:22:22";
-                cmd.Parameters.Add(dateTimeParam);
-
-                MySqlParameter cnlNumParam = new MySqlParameter("@cnlNum", MySqlDbType.Int32);
-                cnlNumParam.Value = 601;
-                cmd.Parameters.Add(cnlNumParam);
-
-                MySqlParameter valParam = new MySqlParameter("@val", MySqlDbType.Double);
-                valParam.Value = 95;
-                cmd.Parameters.Add(valParam);
-
-                cmd.Parameters.Add("@stat", MySqlDbType.Int16).Value = 1;
-
-                int rowCount = cmd.ExecuteNonQuery();
-
-                Console.WriteLine("Row Count affected = " + rowCount);
+                
             }
             catch (Exception e)
             {
@@ -60,11 +79,58 @@ namespace ScadaSimluator
                 conn.Dispose();
                 conn = null;
             }
-
+*/
             Console.Read();
+        }
+        public static void TestSql(int cnlnum, double val)
+        {
+            string sql = "INSERT INTO cnldata (datetime, cnlnum, val, stat) VALUES(@dateTime, @cnlNum, @val, @stat) ON DUPLICATE KEY UPDATE datetime = @dateTime,  val = @val";
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = sql;
 
+                MySqlParameter dateTimeParam = new MySqlParameter("@dateTime", MySqlDbType.String);
+                dateTimeParam.Value = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                cmd.Parameters.Add(dateTimeParam);
 
+                MySqlParameter cnlNumParam = new MySqlParameter("@cnlNum", MySqlDbType.Int32);
+                cnlNumParam.Value = cnlnum;
+                cmd.Parameters.Add(cnlNumParam);
+
+                MySqlParameter valParam = new MySqlParameter("@val", MySqlDbType.Double);
+                valParam.Value = val;
+                cmd.Parameters.Add(valParam);
+
+                cmd.Parameters.Add("@stat", MySqlDbType.Int16).Value = 1;
+
+                int rowCount = cmd.ExecuteNonQuery();
+
+                //Console.WriteLine("Row Count affected = " + rowCount);
 
         }
+        public static double GetQsumm(Spt spt)
+        {
+
+            int num = spt.cnlnum + 7;
+            string sql = "SELECT val FROM cnldata WHERE cnlnum=" + num;
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = sql;
+            string name = cmd.ExecuteScalar().ToString();
+            return Convert.ToDouble(cmd.ExecuteScalar().ToString());
+        }
+
+        public static void SptTobBase(Spt spt)
+        {
+            int num = spt.cnlnum; 
+            TestSql(num, spt.t1);
+            TestSql(++num, spt.t2);
+            TestSql(++num, spt.p1);
+            TestSql(++num, spt.p2);
+            TestSql(++num, spt.g1);
+            TestSql(++num, spt.g2);
+            TestSql(++num, spt.q);
+            TestSql(++num, spt.qsum+spt.q/3600*timeInterval/1000);
+        }
+
+
     }
 }
--- ScadaSimluator/SPT.cs
+++ ScadaSimluator/SPT.cs
@@ -19,7 +19,7 @@ namespace ScadaSimluator
         public double q { get; set;}
         public double qsum { get; set;}
         public Spt (int cnlnum) { this.cnlnum = cnlnum;}
-        public Spt(int cnlnum, double t1, double t2, double p1, double p2, double g1, double g2, double q, double qsum)
+        public Spt(int cnlnum, double t1, double t2, double p1, double p2, double g1)
         {
             this.cnlnum = cnlnum;
             this.t1 = t1;
@@ -28,8 +28,24 @@ namespace ScadaSimluator
             this.p2 = p2;
             this.g1 = g1;
             this.g2 = g2;
-            this.q = q;
-            this.qsum = qsum;
         }
+        public void Random()
+        {
+            Random rnd = new Random();
+            t1 = Math.Round(rnd.Next((int)(t1 * 100 - 10), (int)(t1 * 100 + 10)) * 0.01,2);
+            t2 = Math.Round(rnd.Next((int)(t2 * 100 - 10), (int)(t2 * 100 + 10)) * 0.01,2);
+            p1 = Math.Round(rnd.Next((int)(p1 * 100 - 10), (int)(p1 * 100 + 10)) * 0.01,2);
+            p2 = Math.Round(rnd.Next((int)(p2 * 100 - 10), (int)(p2 * 100 + 10)) * 0.01,2);
+            g1 = Math.Round(rnd.Next((int)(g1 * 100 - 10), (int)(g1 * 100 + 10)) * 0.01,2);
+            g2 = Math.Round(g1-0.1,2);
+            q = Math.Round(g1 * (t1 - t2) / 1000, 3);
+        }
+        public string ToList()
+        {
+            return "clnum= " + cnlnum + "\nt1= " + t1 + "\nt2= " + t2 + "\np1= " + p1 + "\np2= " + p2 + "\ng1= " + g1 + "\ng2= " + g2 + "\nq= " + q + "\nqsum= "+qsum;
+        }
+        
+
+
     }
 }
